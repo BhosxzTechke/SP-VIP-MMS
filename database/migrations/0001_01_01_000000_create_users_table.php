08bf0a57abdeb10d981c74269b7acbda50d11d96
gel-o <gelogelo6403@gmail.com>
2025-07-29 17:00:29 +0800

    new tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,13 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('user_type', ['regular', 'vip', 'admin'])->default('regular');
+            $table->string('referral_code')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
-
-        
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
